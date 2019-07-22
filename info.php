fc2170da2bca4f69e3525e8f203fb10cece80bd7
Ayşenur Kaplan <kaysenur551@gmail.com>
2019-07-23 01:33:18 +0300

    üye ekranı

---
+++ info.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php $A=5; ?>
+<?php if ($A== 5): ?>
+<p>  A değişkeninin değeri 5tir.</p>
+<?php else: ?>
+  İfade doğru değilse bu gösterilir.
+<?php endif; ?>
+
+
+</body>
+  </html>
